512d798d2c0b304bae02c857616ac6ee4e09ebd2
Simon Gate <smgt@buno.se>
2011-07-17 15:04:36 +0200

    Rewrite of database backend

--- tracker.rb
+++ tracker.rb
@@ -1,75 +1,180 @@
+# Not good to pollute the global classes
+# But supersexy code!
+class Fixnum
+  def hours
+    return sprintf("%.2f", self.to_f/60/60).to_f
+  end
+end
+
 class Tracker
 
-  def self.file_name t = nil
-    t = Time.now unless t
-    "data/#{t.year}_#{t.month}_#{t.day}.txt"
-  end
+  # Time lock
+  @@lock_file = "data/clock.lock"
 
-  def self.stop_tracking timestamp
-    if self.running?
-      system "echo 'stop:#{timestamp}' >> #{Tracker.file_name}"
-      "stoping the clock"
-    else
-      "clock is already stopped"
-    end
-  end
+  # Database
+  @@data_file = "data/timetracker.db"
 
-  def self.start_tracking timestamp
-    unless self.running?
-      system "echo 'start:#{timestamp}' >> #{Tracker.file_name}"
-      "starting clock"
-    else
-      "already started"
+  # Workday in seconds
+  @@workday = 28800
+
+  class << self
+
+    def stop(comment="")
+      self.write_tracked_time(comment)
+      return self.unlock_clock if self.locked?
+      return false
     end
 
-  end
-
-  def self.running?
-    action = self.last_line.split(":").first
-    return action.eql? "start"
-  end
-
-  def self.last_line
-    last_line=""
-    return last_line unless File.readable? self.file_name
-    File.open( self.file_name, 'r' ).each_line do |line|
-      last_line = line
+    def start
+      return self.lock_clock unless self.locked?
+      return false
     end
-    last_line
-  end
 
-  def self.total_for t
-    file = self.file_name t
-    return 0 unless File.readable? file
+    def today
+      return self.day(self.date_today)
+    end
 
-    total = started = stopped = 0
-    File.open( file, 'r' ).each_line do |line|
-      s = line.split ":"
-      if s.first.eql?("stop")
-        if started > 0
-          st = s[1].chomp.to_i
-          total += (st - started)
-        else
+    def running?
+      return self.locked?
+    end
+
+    def day(date)
+      file = File.open(@@data_file)
+      today = Array.new
+      file.each do |line|
+        data_array = line.chomp.split(",")
+        if(data_array.first.chomp == date)
+          today << data_array
         end
-        started = 0
+        if(data_array.first.chomp != date)
+          break
+        end
+      end
+      file.close
+      return today
+    end
+
+    def summary(date=nil)
+      if date.nil?
+        date = self.date_today
+      end
+      day = self.day(date)
+      time = 0
+      day.each do |d|
+        time += (d[2].to_i - d[1].to_i)
+      end
+      return {:worked => time, :worked_hours => time.hours, :work_diff => (time - @@workday), :work_diff_hours => (time - @@workday).hours}
+    end
+
+    def last_line
+      #last_line=""
+      #return last_line unless File.readable? self.file_name
+      #File.open( self.file_name, 'r' ).each_line do |line|
+        #last_line = line
+      #end
+      #last_line
+      return "start:1310892473"
+    end
+
+    def total_for t
+      #file = self.file_name t
+      #return 0 unless File.readable? file
+
+      #total = started = stopped = 0
+      #File.open( file, 'r' ).each_line do |line|
+        #s = line.split ":"
+        #if s.first.eql?("stop")
+          #if started > 0
+            #st = s[1].chomp.to_i
+            #total += (st - started)
+          #else
+          #end
+          #started = 0
+        #else
+          #started = s[1].chomp
+          #started = started.to_i
+        #end
+      #end
+      #min  = 60
+      #hour = (60*min)
+      #hours = total / hour
+      #mins = ( total - (hours * hour) ) / min
+      #"#{hours} hours and #{mins} mins (#{total}) seconds"
+      "2 hours and 23 mins (2333 s)"
+    end
+
+    def other_days
+      #d = []
+      #Dir.glob("data/*txt").each do |day|
+        #d << day.gsub('.txt','').gsub('data/','')
+      #end
+      #d
+      return []
+    end
+
+
+  protected 
+
+    def lock_clock
+      if locked?
+        return false
       else
-        started = s[1].chomp
-        started = started.to_i
+        lock = File.new(@@lock_file, "w")
+        lock.puts Time.now.to_i
+        lock.close
+        return true
+      end   
+    end
+
+    def unlock_clock
+      if locked?
+        File.delete(@@lock_file)
+        return true
+      end
+      return false
+    end
+
+    def locked?
+      return File.exists?(@@lock_file);
+    end
+
+    def locked_at?
+      file = File.open(@@lock_file);
+      lock_time = file.readline
+      file.close
+      return lock_time.to_i
+    end
+
+    def last_entry
+      if File.exists?(@@data_file)
+        file = File.open(@@data_file, "r")
+        if entry = file.gets
+          file.close
+          return entry.split(",")
+        end
+      else
+        return false
       end
     end
-    min  = 60
-    hour = (60*min)
-    hours = total / hour
-    mins = ( total - (hours * hour) ) / min
-    "#{hours} hours and #{mins} mins (#{total}) seconds"
-  end
 
-  def self.other_days
-    d = []
-    Dir.glob("data/*txt").each do |day|
-      d << day.gsub('.txt','').gsub('data/','')
+    def write_tracked_time(comment="")
+      if self.locked?
+        last_entry = self.last_entry
+        start_time = self.locked_at?
+        file = File.new(@@data_file, "a+")
+        end_time = Time.now.to_i
+        work_time = end_time - start_time
+        file.puts("#{self.date_today}, #{start_time}, #{end_time}, #{comment}")
+        file.close
+        return true
+      end
+      return false
     end
-    d
-  end
 
+    def date_today
+      return Time.new.strftime("%Y-%m-%d")
+    end
+
+  end
 end
+
--- ts.rb
+++ ts.rb
@@ -4,89 +4,40 @@ require 'haml'
 require 'json'
 require './tracker'
 
-class Time
-  def to_s
-   "#{self.year}-#{self.month}-#{self.day}" 
-  end
-end
-
 set :haml, :format => :html5
 
-def set_total
-  @t = if params[:t]
-        d = params[:t].split("_")
-        Time.new( d[0], d[1], d[2] )
-      else
-        Time.now
-      end
-  @total = Tracker.total_for @t
-  @total_str = "#{@t}:#{@total}"
+get "/", :provides => "html" do
+  @today = Tracker.today
+  @summary = Tracker.summary
+  @running = Tracker.running?
+  haml :index
 end
 
-before do
-  @days = Tracker.other_days
+get "/", :provides => "json" do
+  Tracker.summary.to_json
 end
 
-def resp
-  @msg ||= "#{request.request_method} #{request.path_info}"
-  if request.path_info.eql? '/json'
-    content_type :json
-    {
-      :msg => @msg,
-      :total => "#{@total_str}",
-      :days => @days.collect{|d| "/json?t=#{d}"}
-    }.to_json+"\n"
-  elsif request.path_info.eql? '/txt'
-    content_type :text
-    "#{@msg}\n#{@t}:#{@total_str}\n"
-  else
-    haml :index
+#get "/history*", :provides => "json" do
+  #"json"
+#end
+
+#get "/history*", :provides => "html" do
+  #"html"
+#end
+
+post "/", :provides => ["json", "html", "txt"] do
+  status = "false"
+  if params[:cmd].eql?('start')
+    status = Tracker.start
+  elsif params[:cmd].eql?('stop')
+    status = Tracker.stop
+  end
+  case request.accept.first
+  when "text/html"
+    redirect to("/")
+  when "application/json"
+    status.to_json
+  when "text/plain"
+    statis.to_s
   end
 end
-
-['/', '/json', '/txt'].each do |path|
-  get path do
-    set_total
-    resp
-  end
-end
-
-['/', '/json', '/txt'].each do |path|
-  post path do
-    if params[:cmd].eql?('start')
-      @msg = Tracker.start_tracking Time.now.to_i
-    elsif params[:cmd].eql?('stop')
-      @msg = Tracker.stop_tracking Time.now.to_i
-    end
-    set_total
-    resp
-  end
-end
-
-
-__END__
-
-@@ layout
-%html
-  %head
-    %title ts project
-  %body
-    =yield
-
-@@ index
-%h1
-  time sheet
-  =@name
-.msg
-  =@msg
-.total
-  total time tracked for
-  ="#{@t}: ( #{@total} )"
-%form{:method=>"post"}
-  %input{:type=>"submit", :name=>"cmd", :value=>"start"}
-  %input{:type=>"submit", :name=>"cmd", :value=>"stop"}
-
-%ul
--@days.each do |d|
-  %li
-    ="<a href='/?t=#{d.gsub('.txt','').gsub('data/','')}'>#{d.gsub('.txt','')}</a>"
